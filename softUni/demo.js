468d337985b2a91dde4d917ba7eefd3c31fdd4df
draganovdimitar2 <draganovdimitar2@gmail.com>
2025-08-06 09:00:00 +0300

    Add demo.js with POST, PUT, DELETE fetch examples

---
+++ softUni/demo.js
@@ -0,0 +1,23 @@
+// POST
+await fetch('', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+});
+
+// PUT
+await fetch('', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+});
+
+// DELETE
+await fetch('', {
+    method: 'DELETE'
+});
+
